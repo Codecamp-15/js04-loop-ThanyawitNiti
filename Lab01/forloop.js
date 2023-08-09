f500ef3c629fe7e76b0727bd4f34ac99486498ef
ThanyawitNiti <thanyawit.nitipornwattana@gmail.com>
2023-08-09 23:56:11 +0700

    save

--- Lab01/forloop.js
+++
@@ -1,6 +0,0 @@
-for (begin ; condition ; step) {
-    //loop body
-    // begin ทำแค่ครั้งแรกครั้งเดียวก่อนรันเข้าลูป
-    // condition เช็คก่อน รัน ใน ปีกกา
-    // step พอจบลูปcondtition  รัน step วนกลับไป  condition  เอาไว้อัปเดตค่า
-}

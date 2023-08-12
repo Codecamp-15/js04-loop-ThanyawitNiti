f43d2e56df5687840bee6ac16fa041d0827e9ee0
ThanyawitNiti <thanyawit.nitipornwattana@gmail.com>
2023-08-12 15:55:55 +0700

    js04done

--- 03-forLoop/03-printPattern/main.js
+++ 03-forLoop/03-printPattern/main.js
@@ -1,29 +1,14 @@
-//บรรทัด แรก มี 1 สตาร์
 
-// a ='*'
-// for ( i = * ;)
-// print 4 แถวยังไง
-//let อยู่ข้างนอก ตัวแปรถูก อัปเดตเรื่อยๆ
-//let str='';
-// for (let i = 1 ; i <= 5 ; i++) {
-//     // console.log('*')
-//     let str = '';
-//     for (let j = 1 ; j <= 5 ; j++){
-//         str = str + '*';
-//     }
-//     console.log(str);
-// }
-// star pattern
-// forloop
-// pattern recog
-// for (let i = 1 ; i <= 4 ; i++) {
-//     // console.log('*')
-//     let str = '';
-//     for (let j = 1 ; j <= i ; j++){
-//         str = str + '*';
-//     }
-//     console.log(str);
-// }
+// ให้เขียนโค้ดแสดงรูปดอกจันตามรูปแบบด้านล่าง โดยแสดงผลออกมาใน console
+
+// ```
+// *
+// *	*
+// *	*	*
+// *	*	*	*
+// ```
+
+// หมายเหตุ : คำสั่งขึ้นบรรทัดใหม่ใน console ให้ใช้ \n
 
 let str =''
 for ( let i =1; i <=5 ; i++) {
